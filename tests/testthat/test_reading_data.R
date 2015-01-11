0f5528c59abfa7b316fd9d31aef2e475e114c005
Donne Martin <donne.martin@gmail.com>
2015-01-11 13:15:18 -0500

    Added function to control whether data files are downloaded to speed up unit test execution.  Changed function case to be consistent with other recipes, though inconsistent with test_that

--- tests/testthat/test_reading_data.R
+++ tests/testthat/test_reading_data.R
@@ -3,13 +3,35 @@ context("Reading Data")
 setwd("../../")
 source("reading_data/utilities.R")
 
-getDataDir <- function() {
+GetDataDir <- function() {
+  # Returns the data directory file path.
+  #
+  # Args:
+  #   none
+  #
+  # Returns:
+  #   The data directory file path
   return("data/")
 }
 
+DownloadData <- function() {
+  # Determines whether to download the data files.  Set return value to FALSE
+  # to speed up running the unit tests.
+  #
+  # Args:
+  #   none
+  #
+  # Returns:
+  #   TRUE: Download the data files
+  #   FALSE: Do not download the data files, use the already existing data file
+  #     if it exists.  Note: Does not check whether existing data file already
+  #     exists.
+  return(TRUE)
+}
+
 test_that("data directory is created", {
-  SetupDataDir(getDataDir())
-  expect_true(file.exists(getDataDir()))
+  SetupDataDir(GetDataDir())
+  expect_true(file.exists(GetDataDir()))
 })
 
 test_that("data is downloaded from URL", {
@@ -19,7 +41,7 @@ test_that("data is downloaded from URL", {
   fileDataDest <- "getdata-data-restaurants.xml"
   fileDataDest <- DownloadDataFromUrl(fileDataUrl,
                                       fileDataDest,
-                                      downloadData=TRUE)
+                                      downloadData=DownloadData())
   expect_true(file.exists(fileDataDest))
 })
 
@@ -32,6 +54,8 @@ test_that("data is read from csv", {
                        "getdata%2Fdata%2Fss06hid.csv",
                        sep="")
   fileDataDest <- "getdata-data-ss06hid.csv"
-  result <- ReadCsvFromUrl(fileDataUrl, fileDataDest)
+  result <- ReadCsvFromUrl(fileDataUrl,
+                           fileDataDest,
+                           downloadData=DownloadData())
   expect_equal(result[1], 53)
 })
